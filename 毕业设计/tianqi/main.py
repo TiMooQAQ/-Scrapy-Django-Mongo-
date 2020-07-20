8fc145e695d5b5d57d4fa5b8090bccca14227067
TiMooQAQ <49904816+TiMooQAQ@users.noreply.github.com>
2020-07-21 05:09:24 +0800

    Update main.py

--- 毕业设计/tianqi/main.py
+++ 毕业设计/tianqi/main.py
@@ -1,16 +1,14 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-import time
+import time,datetime
+import os
+
+time_now = time.strftime("%M:%S", time.localtime())  # 刷新
+
 while True:
     time_now = time.strftime("%M:%S", time.localtime())  # 刷新
-    if time_now == "00:30": #此处设置每天定时的时间
+    time.sleep(1)
+    print(f'\r{datetime.datetime.now()}', end='')
 
-
-        if __name__ == '__main__':
-            process = CrawlerProcess(get_project_settings())
-            process.crawl('tianqispider')  # 你需要将此处的spider_name替换为你自己的爬虫名称
-            process.start()
-
-        time.sleep(10)
-
-
+    if time_now == "00:00":  # 此处设置每天定时的时间
+        qq = os.system("scrapy crawl tianqispider")
+        print(qq)
+        time.sleep(20)
