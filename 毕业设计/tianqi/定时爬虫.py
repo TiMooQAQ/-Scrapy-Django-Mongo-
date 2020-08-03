780a97901a0163cc9b984d8b48df47cb185a064a
TiMooQAQ <49904816+TiMooQAQ@users.noreply.github.com>
2020-08-03 17:08:22 +0800

    Update 定时爬虫.py

--- 毕业设计/tianqi/定时爬虫.py
+++ 毕业设计/tianqi/定时爬虫.py
@@ -5,7 +5,7 @@ time_now = time.strftime("%M:%S", time.localtime())  # 刷新
 
 while True:
     time_now = time.strftime("%M:%S", time.localtime())  # 刷新
-    time.sleep(1)
+    time.sleep(0.5)
     print(f'\r{datetime.datetime.now()}', end='')
 
     if time_now == "00:00":  # 此处设置每天定时的时间
